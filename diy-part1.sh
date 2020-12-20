c2226893a66a5d2476745bc6b1c05cfcce381fad
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2020-12-20 23:33:16 +0800

    delete smartdns support

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
