47709efae1985106276ce89b1ac6a3aa04ec0695
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-02-19 22:49:13 +0800

    Update acrh17.sh

--- acrh17.sh
+++ acrh17.sh
@@ -12,4 +12,4 @@
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
