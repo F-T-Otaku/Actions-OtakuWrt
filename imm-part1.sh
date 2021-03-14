4bf799876065b4f4fafedc2ea46f50c741fc69aa
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-03-14 23:29:03 +0800

    Create imm-part1.sh

---
+++ imm-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 F-T-Otaku
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/F-T-Otaku/Action-OpenWrt-x86-r2s-pi4-acrh17
+# File name: imm-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
