150e198d4281dff7e8e6147884b49963270eee78
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-03-14 23:26:16 +0800

    Update and rename acrh17.sh to imm-part2.sh

---
+++ imm-part2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 F-T-Otaku
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/F-T-Otaku/Action-OpenWrt-x86-r2s-pi4-acrh17
+# File name: imm-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+# Modify default Hostname
+sed -i 's/OpenWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
