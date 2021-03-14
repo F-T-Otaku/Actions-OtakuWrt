2c6cb2e1033ae6718ece849dc8b6ef186a8b9733
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-03-14 23:23:23 +0800

    Update and rename diy-part2.sh to lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2021 F-T-Otaku
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# https://github.com/F-T-Otaku/Action-OpenWrt-x86-r2s-pi4-acrh17
+# File name: lede-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+# Modify default Hostname
+sed -i 's/OpenWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
