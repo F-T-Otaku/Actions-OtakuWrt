65155077a8e1a5c318a1b76a169c631f3168b335
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-07-30 20:27:15 +0800

    Update lede-part1.sh

--- lede-part1.sh
+++ lede-part1.sh
@@ -14,7 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
