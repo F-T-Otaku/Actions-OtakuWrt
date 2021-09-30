41d297aa5875700ce48f8b84bbf8c607fb2c8e2f
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-09-30 11:00:13 +0800

    Update imm-part1.sh

--- imm-part1.sh
+++ imm-part1.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
