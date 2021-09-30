d6a1a438e48b585bff29d1b40eb8edb580b2268f
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-09-30 10:52:06 +0800

    Update lede-part1.sh

--- lede-part1.sh
+++ lede-part1.sh
@@ -10,13 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
