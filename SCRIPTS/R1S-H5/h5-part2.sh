baf36657f8ecaea893ff2be1792a31a8214c7701
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-12-13 10:50:07 +0800

    Update h5-part2.sh

--- SCRIPTS/R1S-H5/h5-part2.sh
+++ SCRIPTS/R1S-H5/h5-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.233.1/g' package/base-files/files/bin/config_generate
 # Modify default Hostname
 sed -i 's/ImmortalWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
