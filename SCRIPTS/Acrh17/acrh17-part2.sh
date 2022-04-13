6ee961a8db12740502c5a47c2e94ed60e4e4d4b3
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2022-04-14 01:11:41 +0800

    Update acrh17-part2.sh

--- SCRIPTS/Acrh17/acrh17-part2.sh
+++ SCRIPTS/Acrh17/acrh17-part2.sh
@@ -10,6 +10,21 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Add applications
+# Passwall
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall_package
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall
+cp -rf ./package/lean/passwall_package/* ./package/lean/passwall
+rm -rf ./package/lean/passwall_package
+# Openclash
+git clone -b master https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
+mkdir -p ./package/lean/openclash
+cp -rf ./package/lean/luci-app-openclash/luci-app-openclash/* ./package/lean/openclash
+rm -rf ./package/lean/luci-app-openclash
+# Theme Argon
+rm -rf ./package/feeds/luci/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Modify default Hostname
