e779900fe6f28386bb7190186c354a1164bb40e6
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2022-04-14 01:10:07 +0800

    Update lede-part1.sh

--- SCRIPTS/Acrh17/lede-part1.sh
+++ SCRIPTS/Acrh17/lede-part1.sh
@@ -11,8 +11,5 @@
 #
 
 # Add a feed source
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
