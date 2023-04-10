8a36793d5e4a725f77fda1f908bde0d28dc43f1d
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2023-04-10 21:44:15 +0800

    Update x86-part2.sh
    Use jerrykuku's argon

--- SCRIPTS/X86/x86-part2.sh
+++ SCRIPTS/X86/x86-part2.sh
@@ -27,8 +27,8 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
 git clone -b master --depth=1 https://github.com/QiuSimons/openwrt-mos ./package/mosdns
 # Theme Argon
-# rm -rf ./package/feeds/luci/luci-theme-argon
-# git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
+rm -rf ./package/feeds/luci/luci-theme-argon
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
 git clone -b master --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
