eee0bf4e4f8f5f722ff7b9ad24c6b372226ca010
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2023-09-16 11:06:42 +0800

    Update x86-part1.sh

--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -11,5 +11,9 @@
 #
 
 # Add a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# Add SSRP to openwrt feeds
+#sed -i "/helloworld/d" "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# Add Passwall to openwrt feeds
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
