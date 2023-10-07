72dc24e7672c36e6f8b3b65f9d702b381dc5e75c
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2023-10-07 16:10:53 +0800

    Update m68s-part2.sh

--- SCRIPTS/M68S/m68s-part2.sh
+++ SCRIPTS/M68S/m68s-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2022 F-T-Otaku
+# Copyright (c) 2023 F-T-Otaku
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -11,23 +11,14 @@
 #
 
 # Add applications
-# Passwall
-git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall_package
-git clone -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall
-cp -rf ./package/lean/passwall_package/* ./package/lean/passwall
-rm -rf ./package/lean/passwall_package
-# Openclash
-git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
-mkdir -p ./package/lean/openclash
-cp -rf ./package/lean/luci-app-openclash/luci-app-openclash/* ./package/lean/openclash
-rm -rf ./package/lean/luci-app-openclash
 # Mosdns
-rm -rf ./package/feeds/helloworld/v2ray-geodata
-rm -rf ./package/feeds/packages/mosdns
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
 git clone -b master --depth=1 https://github.com/QiuSimons/openwrt-mos ./package/mosdns
-# Theme Argon config
-# rm -rf ./package/feeds/luci/luci-theme-argon
-# git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
+# Theme Argon
+rm -rf ./package/feeds/luci/luci-theme-argon
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
 git clone -b master --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
