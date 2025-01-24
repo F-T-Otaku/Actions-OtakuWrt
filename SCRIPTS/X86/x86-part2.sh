96c75104c9cfced83cde6e1f698aab4a24c52840
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2025-01-25 02:53:53 +0800

    Update x86-part2.sh
    Remove third-part theme

--- SCRIPTS/X86/x86-part2.sh
+++ SCRIPTS/X86/x86-part2.sh
@@ -10,13 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Add applications
-
-# Theme Argon
-rm -rf ./package/feeds/luci/luci-theme-argon
-git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
-git clone -b master --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+
 # Modify default Hostname
-sed -i 's/OpenWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OtakuWrt/g' package/base-files/files/bin/config_generate
