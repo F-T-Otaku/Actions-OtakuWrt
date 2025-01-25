80bbeaba7f57db938c06f9f16b88f56eed0e417f
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2025-01-25 11:43:15 +0800

    Update m68s-part2.sh

--- SCRIPTS/M68S/m68s-part2.sh
+++ SCRIPTS/M68S/m68s-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 F-T-Otaku
+# Copyright (c) 2025 F-T-Otaku
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -10,17 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Add applications
-# Mosdns
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -f
-git clone -b master --depth=1 https://github.com/QiuSimons/openwrt-mos ./package/mosdns
-# Theme Argon
-rm -rf ./package/feeds/luci/luci-theme-argon
-git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
-git clone -b master --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/luci2/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+
 # Modify default Hostname
-sed -i 's/OpenWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OtakuWrt/g' package/base-files/luci2/bin/config_generate
