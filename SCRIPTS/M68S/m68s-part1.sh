33bdfff07052b87841aad8269fd480ed4173024d
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2025-01-25 11:42:30 +0800

    Update m68s-part1.sh

--- SCRIPTS/M68S/m68s-part1.sh
+++ SCRIPTS/M68S/m68s-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 F-T-Otaku
+# Copyright (c) 2025 F-T-Otaku
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -8,12 +8,13 @@
 # https://github.com/F-T-Otaku/Actions-OtakuWrt
 # File name: m68s-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
 # Add a feed source
-# Add SSRP to openwrt feeds
-#sed -i "/helloworld/d" "feeds.conf.default"
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-# Add Passwall to openwrt feeds
+
+#  Add Passwall to openwrt feeds
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+#  Add SSRP to openwrt feeds
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
